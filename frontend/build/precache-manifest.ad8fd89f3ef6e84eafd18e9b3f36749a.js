d38e20149fd13fc4ad19147b0d54903d4079df19
koakande <koakande@gmail.com>
2020-10-14 21:46:32 +0100

    added build for heroku deployment

---
+++ frontend/build/precache-manifest.ad8fd89f3ef6e84eafd18e9b3f36749a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9875bfc9069df872cd34acf3eb917ff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7015be5a9c7e48d6681",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "68d804fe7e5f60fa42f7",
+    "url": "/static/js/2.5a365f4c.chunk.js"
+  },
+  {
+    "revision": "5fb8ed7e234183dca2c8a48d2d95d882",
+    "url": "/static/js/2.5a365f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7015be5a9c7e48d6681",
+    "url": "/static/js/main.518244d2.chunk.js"
+  },
+  {
+    "revision": "df6af45cb4e7a3480417",
+    "url": "/static/js/runtime-main.3bf9fbca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
