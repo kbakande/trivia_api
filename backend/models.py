123bdb6ed41d19a3943eba410fa3a674a5d41600
koakande <koakande@gmail.com>
2020-10-15 03:58:27 +0100

    added migration

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivi"
+database_name = "trivia"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
@@ -17,7 +17,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
 '''
 Question
 
