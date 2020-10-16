92f2d8f6af24dd3ce657527ccba669c49c2120dd
koakande <koakande@gmail.com>
2020-10-16 21:53:28 +0100

    troubleshooting heroku deployment

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,8 @@ def pagination(request, QUESTIONS_PER_PAGE):
 
 def create_app(test_config=None):
   # create and configure the app
-  app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
+  # app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
+  app = Flask(__name__)
   setup_db(app)
   migrate.init_app(app, db)
 
@@ -41,9 +42,9 @@ def create_app(test_config=None):
     response.headers.add("Access-Control-Allowed-Methods", "GET, PATCH, POST, DELETE, OPTIONS")
     return response
 
-  @app.route('/')
-  def index():
-    return app.send_static_file('index.html')
+  # @app.route('/')
+  # def index():
+  #   return app.send_static_file('index.html')
 
   @app.route('/categories')
   def retrieve_categories():
